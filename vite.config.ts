06aa9ca62a359878432a596af438e1550908183a
Wesley <wes.urb@gmail.com>
2024-12-17 15:35:58 +0100

    chore: format

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,22 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
- plugins: [solidPlugin()],
- build: {
-  lib: {
-   entry: "src/index.ts",
-   name: "solidhelper", // UMD global name
-   formats: ["es", "cjs"], // Build ESM and CommonJS
-   fileName: (format) => `index.${format}.js`,
-  },
-  rollupOptions: {
-   // Exclude solid-js to avoid bundling it
-   external: ["solid-js"],
-   output: {
-    globals: {
-     "solid-js": "SolidJS",
+  plugins: [solidPlugin()],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "solidhelper", // UMD global name
+      formats: ["es", "cjs"], // Build ESM and CommonJS
+      fileName: (format) => `index.${format}.js`,
     },
-   },
+    rollupOptions: {
+      // Exclude solid-js to avoid bundling it
+      external: ["solid-js"],
+      output: {
+        globals: {
+          "solid-js": "SolidJS",
+        },
+      },
+    },
   },
- },
 });
