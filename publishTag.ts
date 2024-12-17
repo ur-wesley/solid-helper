2ee8b52fb6f86dcbffeeeaaf107e8ea15224f235
Wesley <wes.urb@gmail.com>
2024-12-17 15:48:52 +0100

    chore: update publishing tags

--- publishTag.ts
+++ publishTag.ts
@@ -1,8 +1,26 @@
 import { $ } from "bun";
 
 const tag = Bun.argv[2];
+
 if (!tag) {
   throw new Error("TAG is required");
 }
-await $`git tag ${tag}`;
-await $`git push origin ${tag}`;
+
+switch (tag) {
+  case "major":
+    await $`npm version major`;
+    break;
+  case "minor":
+    await $`npm version minor`;
+    break;
+  case "patch":
+    await $`npm version patch`;
+    break;
+  default:
+    throw new Error("Invalid tag");
+}
+
+const version = require("../package.json").version;
+
+await $`git tag ${version}`;
+await $`git push origin ${version}`;
