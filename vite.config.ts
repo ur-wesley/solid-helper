ac4221f835429c18869ae655e49b38c9740becf1
Wesley <wes.urb@gmail.com>
2025-01-15 11:16:56 +0100

    chore: change entry path

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import path from "path";
 
 export default defineConfig({
   plugins: [solidPlugin()],
   build: {
     lib: {
-      entry: "src/index.ts",
+      entry: path.resolve(__dirname, "src/index.ts"),
       name: "solidhelper", // UMD global name
       formats: ["es", "cjs"], // Build ESM and CommonJS
       fileName: (format) => `index.${format}.js`,
